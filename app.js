3ca4040b604e80dd511d1c8aac864b49443f51e2
julia-boot <juliacordero@MacBook-Pro.local>
2020-11-16 17:45:21 -0300

    avances en home

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express"); //Requerimos express
 const app = express(); //Requerimos su funcion de alto nivel
 const path = require('path'); //Requerimos path
+const mainRouter = require('./src/routers/mainRouter')
 
 app.set("view engine", "ejs") //Aclaramos cual sera el motor del template
 app.set('views', path.resolve(__dirname, 'src','views')); //Aclaramos la carpeta vistas
 app.use(express.static("public")); //Definimos una carpeta  estatica: public.
 
-app.get("/", function(req, res){ // Creamos rutas.
-    res.render("index"); //Renderizamos la vista en "index" (no hace falta .ejs)
-})
+app.get("/", mainRouter);
 
 app.listen(3000, function(){ //Corremos el server en escucha
     console.log("El servidor está corriendo en el puerto 3000")
