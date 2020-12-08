ad45a10d3614bd3e271dafcd96251311fc465a41
julia-boot <juliacordero@MacBook-Pro.local>
2020-12-08 18:06:33 -0300

    .env

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
 const express = require("express"); //Requerimos express
 const app = express(); //Requerimos su funcion de alto nivel
 const path = require('path'); //Requerimos path
 const mainRouter = require('./src/routers/mainRouter');
 const userRouter = require('./src/routers/userRouter');
+const port = process.env.PORT || 3000; 
 
 app.set("view engine", "ejs") //Aclaramos cual sera el motor del template
 app.set('views', path.resolve(__dirname, 'src','views')); //Aclaramos la carpeta vistas
 app.use(express.static("public")); //Definimos una carpeta  estatica: public.
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 app.use("/", mainRouter);
 app.use("/user", userRouter);
 
 
-app.listen(3000, function(){ //Corremos el server en escucha
-    console.log("El servidor está corriendo en el puerto 3000")
-})
-
+app.listen(port)
